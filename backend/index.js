95224ffaebf5241f34a9e0ece5f65f2025c375dc
Keri Gerheart <kerigerheart@gmail.com>
2025-07-22 21:25:15 -0400

    added testing for connection to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 app.use(express.json());
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGODB_URI).then(() => console.log("MongoDB connected"));
 
 app.get("/", (req, res) => res.send("API is running"));
 app.listen(3001, () => console.log("Server running on port 3001"));
