14b8e9594f174312424d8426ba75c45db11ce6fe
Keri Gerheart <kerigerheart@gmail.com>
2025-07-26 18:06:31 -0400

    finished header layout with states for signed in or out

--- frontend/src/app/components/Header.js
+++ frontend/src/app/components/Header.js
@@ -5,6 +5,7 @@ import { Dialog, DialogPanel } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import Link from "next/link";
+import { SignedIn, SignedOut, SignInButton, SignUpButton } from "@clerk/nextjs";
 
 const navigation = [
     { name: "Home", href: "#" },
@@ -17,13 +18,13 @@ export default function Example() {
 
     return (
         <header className="bg-white font-body">
-            <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 gap-8 lg:px-8">
+            <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 gap-8 md:px-8">
                 <Link href="/" className="flex items-end -translate-y-1">
                     <Image src="/linklocker.svg" alt="LinkLocker Logo" width={40} height={40} />
                     <span className="text-lg text-dark_grey font-semibold">LinkLocker</span>
                 </Link>
 
-                <div className="flex lg:hidden">
+                <div className="flex md:hidden">
                     <button
                         type="button"
                         onClick={() => setMobileMenuOpen(true)}
@@ -32,29 +33,32 @@ export default function Example() {
                         <Bars3Icon aria-hidden="true" className="size-6" />
                     </button>
                 </div>
-                <div className="hidden lg:flex lg:gap-x-12">
-                    {navigation.map((item) => (
-                        <a
-                            key={item.name}
-                            href={item.href}
-                            className="text-sm/6 font-semibold text-dark_grey hover:text-primary_blue transition-colors duration-200">
-                            {item.name}
-                        </a>
-                    ))}
+
+                <div className="hidden md:flex md:gap-x-12">
+                    <SignedIn>
+                        {navigation.map((item) => (
+                            <Link
+                                key={item.name}
+                                href={item.href}
+                                className="text-sm/6 font-semibold text-dark_grey hover:text-primary_blue transition-colors duration-200">
+                                {item.name}
+                            </Link>
+                        ))}
+                    </SignedIn>
+                    <SignedOut>
+                        <SignInButton className="text-sm/6 font-semibold text-primary_blue hover:text-dark_grey transition-colors duration-200" />
+                        <SignUpButton className="text-sm/6 font-semibold text-primary_blue hover:text-dark_grey transition-colors duration-200" />
+                    </SignedOut>
                 </div>
             </nav>
             <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
                 <div className="fixed inset-0 z-50" />
                 <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                     <div className="flex items-center justify-between">
-                        <a href="#" className="-m-1.5 p-1.5">
-                            <span className="sr-only">Your Company</span>
-                            <img
-                                alt=""
-                                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-                                className="h-8 w-auto"
-                            />
-                        </a>
+                        <Link href="/" className="flex items-end -translate-y-1">
+                            <Image src="/linklocker.svg" alt="LinkLocker Logo" width={40} height={40} />
+                            <span className="text-lg text-dark_grey font-semibold">LinkLocker</span>
+                        </Link>
                         <button
                             type="button"
                             onClick={() => setMobileMenuOpen(false)}
@@ -66,14 +70,20 @@ export default function Example() {
                     <div className="mt-6 flow-root">
                         <div className="-my-6 divide-y divide-gray-500/10">
                             <div className="space-y-2 py-6">
-                                {navigation.map((item) => (
-                                    <a
-                                        key={item.name}
-                                        href={item.href}
-                                        className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
-                                        {item.name}
-                                    </a>
-                                ))}
+                                <SignedIn>
+                                    {navigation.map((item) => (
+                                        <Link
+                                            key={item.name}
+                                            href={item.href}
+                                            className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900">
+                                            {item.name}
+                                        </Link>
+                                    ))}
+                                </SignedIn>
+                                <SignedOut>
+                                    <SignInButton className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-primary_blue" />
+                                    <SignUpButton className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-primary_blue" />
+                                </SignedOut>
                             </div>
                         </div>
                     </div>
