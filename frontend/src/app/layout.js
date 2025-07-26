30bf991aa9d7f593eec5c1a76a312dbb1e62cbec
Keri Gerheart <kerigerheart@gmail.com>
2025-07-26 18:47:41 -0400

    reorganized file structure to move componments out of app dir, updated tailwind config

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,8 +1,8 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { Roboto, Montserrat } from "next/font/google";
 import "./globals.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const roboto = Roboto({
     subsets: ["latin"],
