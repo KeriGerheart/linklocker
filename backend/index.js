ffeb4d210030b25f43686027d43c59c9f85c0576
Keri Gerheart <kerigerheart@gmail.com>
2025-08-02 13:26:25 -0400

    added api route to create new locker

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-app.use(express.json());
-
+const cors = require("cors");
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGODB_URI).then(() => console.log("MongoDB connected"));
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.error("MongoDB connection error:", err));
+
+const lockerRoutes = require("./routes/lockers");
+
+app.use("/api/lockers", lockerRoutes);
 
 app.get("/", (req, res) => res.send("API is running"));
 app.listen(3001, () => console.log("Server running on port 3001"));
---
+++ backend/routes/lockers.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const { nanoid } = require("nanoid");
+const Locker = require("../models/Locker");
+
+const router = express.Router();
+
+//create a new locker
+router.post("/", async (req, res) => {
+    try {
+        const { title, destinationUrl, password, expirationDays, ownerId } = req.body;
+
+        if (!title || !destinationUrl || !expirationDays || !ownerId) {
+            return res.status(400).json({ error: "Missing required fields" });
+        }
+
+        let passwordHash;
+        if (password && password.trim() !== "") {
+            passwordHash = await bcrypt.hash(password, 10);
+        }
+
+        const shortCode = nanoid(6);
+
+        const expirationDate = new Date();
+        expirationDate.setDate(expirationDate.getDate() + Number(expirationDays));
+
+        const newLocker = new Locker({
+            title,
+            destinationUrl,
+            shortCode,
+            passwordHash,
+            expirationDate,
+            ownerId,
+        });
+
+        await newLocker.save();
+
+        return res.status(201).json({
+            message: "Locker created!",
+            locker: {
+                id: newLocker._id,
+                shortCode: newLocker.shortCode,
+                expirationDate: newLocker.expirationDate,
+            },
+        });
+    } catch (error) {
+        console.error("Error creating locker:", error);
+        res.status(500).json({ error: "Server Error" });
+    }
+});
+
+module.exports = router;
